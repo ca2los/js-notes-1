dadff061ec39220ce008cb91d101cdaaa67e59ac
ca2los <6784754+ca2los@users.noreply.github.com>
2024-12-16 21:52:40 -0600

    Optimized the "waitress" script.

--- 2-tip-calculator.js
+++ 2-tip-calculator.js
@@ -9,42 +9,28 @@ const REG_TIP = .13
 const TOP_TIP = .15
 
 function checkout(total) {
-    if (tip === LOW_TIP) {
-        total = total + (total * LOW_TIP)
+    if (tip === LOW_TIP || tip === REG_TIP || tip === TOP_TIP) {
+        total += (total * tip)
         console.log("Total: USD$", total)
-        checkoutHostess(total)
-    } else if (tip === REG_TIP) {
-        total = total + (total * REG_TIP)
-        console.log("Total: USD$", total)
-        checkoutHostess(total)
-    } else if (tip === TOP_TIP) {
-        total = total + (total * TOP_TIP)
-        console.log("Total: USD$", total)
-        checkoutHostess(total)
+        console.log("Tip of", tip,"% included.")
+        checkoutHosts(total)
     } else {
-        console.log("Define the tip.")
+        console.log("Define the value of tip.")
     }
 }
 
-function checkoutHostess(total) {
-    while (true) {
-        if (hostess > 1) {
-            console.log("Hostess paying: ", Number(hostess))
-            console.log("Each host: USD $", (total/Number(hostess)).toFixed(2))
-            break
-        } else if (Number(hostess) === 1) {
-            console.log("Paid by one host.")
-            break
-        } else if (Number(hostess) <= 0) {
-            console.log("Define hostess.")
-            break
-        } else {
-            console.log("Unreachable operation!")
-        }
+function checkoutHosts(total) {
+    if (hosts > 1) {
+        console.log("Split the check in", Number(hosts), "hosts.")
+        console.log("USD $", (total/Number(hosts)).toFixed(2))
+    } else if (Number(hosts) === 1) {
+        console.log("Don't split the check.")
+    } else {
+        console.log("Define the value of hosts.")
     }
 }
 
 let ticket = "400.00"   // Input value (always string) by default is "0".
-let hostess = "6"       // Input value (always string) by default is "0".
+let hosts = "1"       // Input value (always string) by default is "0".
 let tip = TOP_TIP       // Constant value (selected) by default is "REG_TIP"
 checkout(Number(ticket));
