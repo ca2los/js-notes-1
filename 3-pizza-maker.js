645e8e80fc777c6df37d22ff806e68b1658f4826
ca2los <6784754+ca2los@users.noreply.github.com>
2024-12-23 22:06:05 -0600

    Pizza maker first version.

---
+++ 3-pizza-maker.js
@@ -0,0 +1,57 @@
+"use strict";
+// Pizza Maker
+// Give options to create a custom pizza
+// Create a new value with the new pizza
+// Calculate the total cost in dollars
+
+const DELIVERY_TIP = .12
+
+function selectIngredients(hasType) {
+    let ingredients = {
+        mozzarella: 4,
+        mushrooms: 3,
+        olives: 3,
+        onion: 2,
+        pepperoni: 2,
+        salami: 2,
+        sausage: 2
+    }
+    let customPizza = hasType + ingredients.mozzarella + ingredients.mushrooms
+    let total = customPizza + (customPizza * DELIVERY_TIP)
+    console.log("USD $", total.toFixed(2))
+}
+
+function checkoutOrder (hasType) {
+    let customize = prompt("Do you want to add more ingredients?")
+    if (customize.toUpperCase() === "YES" || customize.toUpperCase() === "Y") {
+        selectIngredients(hasType)
+    } else {
+        let total = hasType + (hasType * DELIVERY_TIP)
+        console.log("USD $", total.toFixed(2))
+    }
+}
+
+let pizza = {
+    cheese: 21.50,
+    hawaiian: 20.50,
+    italian: 25.99,
+    mexican: 22.99,
+    traditional: 23.50
+}
+let selection = pizza["italian"]
+checkoutOrder(selection)
+
+
+/**let obj = {        // Object storing compound values
+    a: "Hello World",       // Property with a string value
+    b: 1986,                // Property with a number value
+    c: true,                // Property with a boolean value
+    d: undefined,           // Property with an undefined value
+    e: null                 // Property with a null (object) value
+}
+
+let f = "e";       // New variable to access an object's property
+
+console.log(obj.a,obj["b"],obj.c,obj["d"],obj.e) // Access to the object's properties
+console.log(obj[f]) // The new variable access to one of the object´s properties
+console.log(f) // The new variable will retrieve the string and not the object's value**/
