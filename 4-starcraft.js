bd58b4f232c6024524702241ef2a14ef5209e0ba
ca2los <6784754+ca2los@users.noreply.github.com>
2025-01-26 13:49:58 -0600

    Learning map() and callbacks() with StarCraft.

---
+++ 4-starcraft.js
@@ -0,0 +1,23 @@
+"use strict"
+// StarCraft
+// Retrieve units statistics
+// Confront units and retrieve updated statistics
+
+const terran = [
+    {
+        name: "firebat",
+        health: 50,
+        armor: 1,
+        attack_air: 0,
+        attack_ground: 8,
+        attack_range: 2
+    },
+    {
+        name: "marine",
+        health: 40,
+        armor: 0,
+        attack_air: 6,
+        attack_ground: 6,
+        attack_range: 4
+    }
+]
